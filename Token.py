d4a610bb0bd88ea4e5fc56cd346637c5b188ce1e
Nikita Smith <nikitasmith6@gmail.com>
2023-02-23 16:28:18 -0500

    Thursday Updates

--- Token.py
+++ Token.py
@@ -1,13 +1,74 @@
 #Tokens
-keywords = ['import', 'implementations', 'function', 'main', 'return',
+import json
+
+class keywords:
+    keywords = ['import', 'implementations', 'function', 'main', 'return',
             'type', 'integer', 'double', 'char', 'num', 'is', 'variables',
             'define', 'of', 'begin', 'display', 'set', 'exit', 'endfun',
             'symbol', 'end', 'input', 'structures', 'pointer', 'head',
             'last', 'NULL', 'ChNode', 'using', 'reverse', 'while',
             'endwhile', 'call'] #KEYWORDS from token document (many one off tokens (such as main) from the document have been consolidated under keywords)
-identifiers = ['x', 'r',  'area', 'cir', 'pchar', 'j']#CONST from the token document
-operators = ['+', '-', '*', '/', '^', '>', '<', '"', '='] #OPERATORS from token document (= was made into an operator as opposed
-literals = [ '45.95', 'Welcome to the world of SCL', 'Value of x:', 'Enter value of radius:',
-            'Value of radius:', 'Value of area:', 'Value of circumference:',
-            'scl.h', 'stdio.h', 'stdlib.h', 'string.h', 'recrevlisth.c'] #STATEMENTS from the token document
-specialSymbols = [',', '.', 'PI', 'M_PI'] #PI is a statement, M_PI is the value of PI? (leave as is for now...)
+    keywordsID = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                  17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                  32] #33
+
+
+class identifiers:
+    identifiers = ['x', 'r',  'area', 'cir', 'pchar', 'j']#CONST from the token document
+    identifiersID = [200, 201, 202, 203, 204, 205]
+
+class operators:
+    operators = ['+', '-', '*', '/', '^', '>', '<', '"', '='] #OPERATORS from token document (= was made into an operator as opposed
+    operatorsID = [400, 401, 402, 403, 404, 405, 406, 407, 408,]
+class literals:
+    literals = [] #STATEMENTS from the token document
+            #string literals occur after the tokens 'input' or 'display
+            #header (and .c) literals occur after the token 'import'
+            #'49.95' occurs after 'set' 'x' '='
+    literalsID = [600]
+class specialSymbols:
+    specialSymbols = [',', '.', 'PI', 'M_PI'] #PI is a statement, M_PI is the value of PI? (leave as is for now...)
+    specialSymbolsID = [800, 801, 802, 803, 804]
+
+
+
+#for each x in lineList
+    #for each i in keywords
+        #if lineList[x] == keywords[i]
+            #print('Next token is:' + keywordsID[i] + 'Next lexeme is ' + lineList[i]
+    #for each i in identifiers
+        #if lineList[x] == identifiers[i]
+            #print('Next token is:' + identifiersID[i] + 'Next lexeme is ' + lineList[i]
+    #for each i in operators
+        #if lineList[x] == operators[i]
+            #print('Next token is:' + operatorsID[i] + 'Next lexeme is ' + lineList[i]
+    #for each i in literals
+        #if lineList[x] == literals[i]
+            #print('Next token is:' + literalsID[0] + 'Next lexeme is ' + lineList[i]
+    #for each i in specialSymbols
+        #if lineList[x] == specialSymbols[i]
+            #print('Next token is:' + specialSymbolsID[i] + 'Next lexeme is ' + lineList[i]
+
+
+    #writing token list to json: https://pythonexamples.org/python-write-json-to-file/
+    #import json
+    #aDict = {tokensHere} #?
+    # jsonString
+   # if __name__ == '__main__':
+      #  Area_cir = filter_file("areacir.scl")
+
+
+#?Create a dynamic array that records tokens as they are encountered in the given file, Then
+jsonString = json.dumps(keywords.keywords)
+#jsonString = json.dumps(keywords.keywords, identifiers.identifiers, operators.operators, literals.literals, specialSymbols.specialSymbols)
+#jsonString = json.dumps(keywords, identifiers, operators, literals, specialSymbols)
+jsonFile = open ("tokenList", "w")
+jsonFile.write(jsonString)
+jsonString = json.dumps(identifiers.identifiers)
+jsonFile.write(jsonString)
+jsonString = json.dumps(operators.operators)
+jsonFile.write(jsonString)
+jsonString = json.dumps(literals.literals)
+jsonFile.write(jsonString)
+jsonString = json.dumps(specialSymbols.specialSymbols) #Find a better way
+jsonFile.close()
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import Token
+kw = Token.keywords()
+id = Token.identifiers()
+op = Token.operators()
+lit = Token.literals()
+spS = Token.specialSymbols()
+
 def remove_items(test_list, item):
     # using list comprehension to perform the task
     res = [i for i in test_list if i != item]
@@ -5,11 +12,8 @@ def remove_items(test_list, item):
     return res
 
 
-# group 6 (Allen Smith, Nikita Smith, Josh Poore, Cole Young)
-# TODO: Make this into a function that will be called by the token creator
-if __name__ == '__main__':
-    file = open('areacir.scl', 'r')
-
+def filter_file(File_name):
+    file = open(File_name, 'r')
     # filtering off:
     # Block Comments
     # Empty Lines
@@ -20,6 +24,28 @@ if __name__ == '__main__':
     lineList = []
 
     for line in file:
+        lineTokens = []
+        if '"' in line:
+            splitLocation = line.find('"')
+            beforeStr = line[:splitLocation]
+            afterStr = line[splitLocation:]
+            secondSplitLocation = splitLocation + afterStr[1:].find('"') + 1
+            thirdSplitLocation = afterStr[1:].find('"')
+            strStatement = line[splitLocation:secondSplitLocation + 1]
+            afterStr = line[secondSplitLocation + 1:]
+
+            beforeStatementTokens = beforeStr.split(' ')
+            for token in beforeStatementTokens:
+                lineTokens.append(token)
+
+            lineTokens.append(strStatement)
+
+            afterStatementTokens = afterStr.split(' ')
+            for token in afterStatementTokens:
+                lineTokens.append(token)
+
+            lineList.append(lineTokens)
+            continue
 
         lineTokens = line.split(' ')
 
@@ -35,6 +61,25 @@ if __name__ == '__main__':
 
         if commentEnd in line:
             Comment = False
+        # End of Block comment filter
+
+
+    # for line in file:
+    #
+    #     lineTokens = line.split(' ')
+    #
+    #     # Filters Block Comments (Proof of Concept, need to implement temp for changes)
+    #     commentStart = "/*"
+    #     commentEnd = "*/"
+    #
+    #     if commentStart in line:
+    #         Comment = True
+    #
+    #     if not Comment:
+    #         lineList.append(lineTokens)
+    #
+    #     if commentEnd in line:
+    #         Comment = False
         # End of Block comment filter
 
         # Need to do \n thing here
@@ -70,27 +115,55 @@ if __name__ == '__main__':
     lineList = list(filter(None, lineList))
 
     # Split '"' into separate tokens
-    loopCount = 0
-    for line in lineList:
-        newLine = []
-
-        for token in line:
-            # covers if " is at the beginning of the token
-            if '"' in token[0] and len(token) > 1:
-                idx = token.index('"')
-                newLine.append('"')
-                newLine.append(token[1:])
-            else:
-                newLine.append(token)
-
-            # covers if " is at the end of the token
-            if '"' in newLine[len(newLine) - 1]:
-                newLine[len(newLine) - 1] = newLine[len(newLine) - 1].removesuffix('"')
-                newLine.append('"')
-
-        lineList[loopCount] = newLine
-        loopCount += 1
+    # loopCount = 0
+    # for line in lineList:
+    #     newLine = []
+    #
+    #     for token in line:
+    #         # covers if " is at the beginning of the token
+    #         if '"' in token[0] and len(token) > 1:
+    #             idx = token.index('"')
+    #             newLine.append('"')
+    #             newLine.append(token[1:])
+    #         else:
+    #             newLine.append(token)
+    #
+    #         # covers if " is at the end of the token
+    #         if '"' in newLine[len(newLine) - 1]:
+    #             newLine[len(newLine) - 1] = newLine[len(newLine) - 1].removesuffix('"')
+    #             newLine.append('"')
+    #
+    #     lineList[loopCount] = newLine
+    #     loopCount += 1
 
     # print final filtered list
     for line in lineList:
         print(line)
+
+    return lineList
+
+# def printToken(fileList, kw, id, op, lit, spS)
+#     for each x in lineList
+#         for each i in keywords
+#                 if lineList[x] == keywords[i]
+#                 print('Next token is:' + keywordsID[i] + 'Next lexeme is ' + lineList[i]
+#         for each i in identifiers
+#             if lineList[x] == identifiers[i]
+#                 print('Next token is:' + identifiersID[i] + 'Next lexeme is ' + lineList[i]
+#         for each i in operators
+#             if lineList[x] == operators[i]
+#                 print('Next token is:' + operatorsID[i] + 'Next lexeme is ' + lineList[i]
+#         for each i in literals
+#             if lineList[x] == literals[i]
+#                 print('Next token is:' + literalsID[0] + 'Next lexeme is ' + lineList[i]
+#         for each i in specialSymbols
+#             if lineList[x] == specialSymbols[i]
+#                 print('Next token is:' + specialSymbolsID[i] + 'Next lexeme is ' + lineList[i]
+
+
+
+# group 6 (Allen Smith, Nikita Smith, Josh Poore, Cole Young)
+# TODO: Make this into a function that will be called by the token creator
+if __name__ == '__main__':
+    Area_cir = filter_file("welcome.scl")
+
