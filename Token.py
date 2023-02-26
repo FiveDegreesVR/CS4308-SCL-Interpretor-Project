cca03d4961a9ca95dc6dfca51f89b06df1922a7d
FiveDegreesVR <nikitasmith6@gmail.com>
2023-02-26 01:45:38 -0500

    ahhhhhhhhhhhhhhhh

--- Token.py
+++ Token.py
@@ -1,74 +1,157 @@
-#Tokens
+# Tokens
 import json
 
-class keywords:
-    keywords = ['import', 'implementations', 'function', 'main', 'return',
-            'type', 'integer', 'double', 'char', 'num', 'is', 'variables',
-            'define', 'of', 'begin', 'display', 'set', 'exit', 'endfun',
-            'symbol', 'end', 'input', 'structures', 'pointer', 'head',
-            'last', 'NULL', 'ChNode', 'using', 'reverse', 'while',
-            'endwhile', 'call'] #KEYWORDS from token document (many one off tokens (such as main) from the document have been consolidated under keywords)
-    keywordsID = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-                  17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-                  32] #33
+
+# class keywords:
+#     keywords = ['import', 'implementations', 'function', 'main', 'return',
+#                 'type', 'integer', 'double', 'char', 'num', 'is', 'variables',
+#                 'define', 'of', 'begin', 'display', 'set', 'exit', 'endfun',
+#                 'symbol', 'end', 'input', 'structures', 'pointer', 'head',
+#                 'last', 'NULL', 'ChNode', 'using', 'reverse', 'while',
+#                 'endwhile',
+#                 'call']  # KEYWORDS from token document (many one off tokens (such as main) from the document have been consolidated under keywords)
+#     keywordsID = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+#                   17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+#                   32]  # 33
+#
+#
+# class identifiers:
+#     identifiers = ['x', 'r', 'area', 'cir', 'pchar', 'j']  # CONST from the token document
+#     identifiersID = [200, 201, 202, 203, 204, 205]
+#
+#
+# class operators:
+#     operators = ['+', '-', '*', '/', '^', '>', '<', '"',
+#                  '=']  # OPERATORS from token document (= was made into an operator as opposed
+#     operatorsID = [400, 401, 402, 403, 404, 405, 406, 407, 408, ]
+#
+#
+# class literals:
+#     literals = []  # STATEMENTS from the token document
+#     # string literals occur after the tokens 'input' or 'display
+#     # header (and .c) literals occur after the token 'import'
+#     # '49.95' occurs after 'set' 'x' '='
+#     literalsID = [600]
+#
+#
+# class specialSymbols:
+#     specialSymbols = [',', '.', 'PI', 'M_PI']  # PI is a statement, M_PI is the value of PI? (leave as is for now...)
+#     specialSymbolsID = [800, 801, 802, 803, 804]
 
 
-class identifiers:
-    identifiers = ['x', 'r',  'area', 'cir', 'pchar', 'j']#CONST from the token document
-    identifiersID = [200, 201, 202, 203, 204, 205]
-
-class operators:
-    operators = ['+', '-', '*', '/', '^', '>', '<', '"', '='] #OPERATORS from token document (= was made into an operator as opposed
-    operatorsID = [400, 401, 402, 403, 404, 405, 406, 407, 408,]
-class literals:
-    literals = [] #STATEMENTS from the token document
-            #string literals occur after the tokens 'input' or 'display
-            #header (and .c) literals occur after the token 'import'
-            #'49.95' occurs after 'set' 'x' '='
-    literalsID = [600]
-class specialSymbols:
-    specialSymbols = [',', '.', 'PI', 'M_PI'] #PI is a statement, M_PI is the value of PI? (leave as is for now...)
-    specialSymbolsID = [800, 801, 802, 803, 804]
-
-
-
-#for each x in lineList
-    #for each i in keywords
-        #if lineList[x] == keywords[i]
-            #print('Next token is:' + keywordsID[i] + 'Next lexeme is ' + lineList[i]
-    #for each i in identifiers
-        #if lineList[x] == identifiers[i]
-            #print('Next token is:' + identifiersID[i] + 'Next lexeme is ' + lineList[i]
-    #for each i in operators
-        #if lineList[x] == operators[i]
-            #print('Next token is:' + operatorsID[i] + 'Next lexeme is ' + lineList[i]
-    #for each i in literals
-        #if lineList[x] == literals[i]
-            #print('Next token is:' + literalsID[0] + 'Next lexeme is ' + lineList[i]
-    #for each i in specialSymbols
-        #if lineList[x] == specialSymbols[i]
-            #print('Next token is:' + specialSymbolsID[i] + 'Next lexeme is ' + lineList[i]
+# for each x in lineList
+# for each i in keywords
+# if lineList[x] == keywords[i]
+# print('Next token is:' + keywordsID[i] + 'Next lexeme is ' + lineList[i]
+# for each i in identifiers
+# if lineList[x] == identifiers[i]
+# print('Next token is:' + identifiersID[i] + 'Next lexeme is ' + lineList[i]
+# for each i in operators
+# if lineList[x] == operators[i]
+# print('Next token is:' + operatorsID[i] + 'Next lexeme is ' + lineList[i]
+# for each i in literals
+# if lineList[x] == literals[i]
+# print('Next token is:' + literalsID[0] + 'Next lexeme is ' + lineList[i]
+# for each i in specialSymbols
+# if lineList[x] == specialSymbols[i]
+# print('Next token is:' + specialSymbolsID[i] + 'Next lexeme is ' + lineList[i]
 
 
-    #writing token list to json: https://pythonexamples.org/python-write-json-to-file/
-    #import json
-    #aDict = {tokensHere} #?
-    # jsonString
-   # if __name__ == '__main__':
-      #  Area_cir = filter_file("areacir.scl")
+# writing token list to json: https://pythonexamples.org/python-write-json-to-file/
+# import json
+# aDict = {tokensHere} #?
+# jsonString
+# if __name__ == '__main__':
+#  Area_cir = filter_file("areacir.scl")
 
 
-#?Create a dynamic array that records tokens as they are encountered in the given file, Then
-jsonString = json.dumps(keywords.keywords)
-#jsonString = json.dumps(keywords.keywords, identifiers.identifiers, operators.operators, literals.literals, specialSymbols.specialSymbols)
-#jsonString = json.dumps(keywords, identifiers, operators, literals, specialSymbols)
-jsonFile = open ("tokenList", "w")
-jsonFile.write(jsonString)
-jsonString = json.dumps(identifiers.identifiers)
-jsonFile.write(jsonString)
-jsonString = json.dumps(operators.operators)
-jsonFile.write(jsonString)
-jsonString = json.dumps(literals.literals)
-jsonFile.write(jsonString)
-jsonString = json.dumps(specialSymbols.specialSymbols) #Find a better way
-jsonFile.close()
+# ?Create a dynamic array that records tokens as they are encountered in the given file, Then
+# jsonString = json.dumps(keywords.keywords)
+# jsonString = json.dumps(keywords.keywords, identifiers.identifiers, operators.operators, literals.literals, specialSymbols.specialSymbols)
+# jsonString = json.dumps(keywords, identifiers, operators, literals, specialSymbols)
+# jsonFile = open ("tokenList", "w")
+# jsonFile.write(jsonString)
+# jsonString = json.dumps(identifiers.identifiers)
+# jsonFile.write(jsonString)
+# jsonString = json.dumps(operators.operators)
+# jsonFile.write(jsonString)
+# jsonString = json.dumps(literals.literals)
+# jsonFile.write(jsonString)
+# jsonString = json.dumps(specialSymbols.specialSymbols) #Find a better way
+# jsonFile.close()
+
+tokenList = {
+    "keywords": {
+        'import': 0,
+        'implementations': 1,
+        'function': 2,
+        'main': 3,
+        'return': 4,
+        'type': 5,
+        'integer': 6,
+        'double': 7,
+        'char': 8,
+        'num': 9,
+        'is': 10,
+        'variables': 11,
+        'define': 12,
+        'of': 13,
+        'begin': 14,
+        'display': 15,
+        'set': 16,
+        'exit': 17,
+        'endfun': 18,
+        'symbol': 19,
+        'end': 20,
+        'input': 21,
+        'structures': 22,
+        'pointer': 23,
+        'head': 24,
+        'last': 25,
+        'NULL': 26,
+        'ChNode': 27,
+        'using': 28,
+        'reverse': 29,
+        'while': 30,
+        'endwhile': 31,
+        'call': 32
+    },
+
+    "identifiers": {
+        'x': 200,
+        'r': 201,
+        'area': 202,
+        'cir': 203,
+        'pchar': 204,
+        'j': 205
+    },
+
+    "operators": {
+        '+': 400,
+        '-': 401,
+        '*': 402,
+        '/': 403,
+        '^': 404,
+        '>': 405,
+        '<': 406,
+        '"': 407,
+        '=': 408
+    },
+
+    "specialSymbols": {
+        ',': 800,
+        '.': 801,
+        'PI': 802,
+        'M_PI': 803
+    }
+}
+
+
+class Token:
+    def __init__(self, type, id, value):
+        self.type = type
+        self.id = id
+        self.value = value
+
+    def getData(self):
+        return [self.type, self.id, self.value]
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import Token
-kw = Token.keywords()
-id = Token.identifiers()
-op = Token.operators()
-lit = Token.literals()
-spS = Token.specialSymbols()
+from Token import *
+import json
+
+
+# kw = Token.keywords()
+# id = Token.identifiers()
+# op = Token.operators()
+# lit = Token.literals()
+# spS = Token.specialSymbols()
 
 def remove_items(test_list, item):
     # using list comprehension to perform the task
@@ -48,6 +51,28 @@ def filter_file(File_name):
             lineList.append(lineTokens)
             continue
 
+        if '^' in line:
+            splitLocation = line.find('^')
+            beforeStr = line[:splitLocation]
+            afterStr = line[splitLocation:]
+            secondSplitLocation = splitLocation + afterStr[1:].find('^') + 1
+            thirdSplitLocation = afterStr[1:].find('^')
+            strStatement = line[splitLocation:secondSplitLocation + 1]
+            afterStr = line[secondSplitLocation + 1:]
+
+            beforeStatementTokens = beforeStr.split(' ')
+            for token in beforeStatementTokens:
+                lineTokens.append(token)
+
+            lineTokens.append(strStatement)
+
+            afterStatementTokens = afterStr.split(' ')
+            for token in afterStatementTokens:
+                lineTokens.append(token)
+
+            lineList.append(lineTokens)
+            continue
+
         lineTokens = line.split(' ')
 
         # Filters Block Comments (Proof of Concept, need to implement temp for changes)
@@ -63,7 +88,6 @@ def filter_file(File_name):
         if commentEnd in line:
             Comment = False
         # End of Block comment filter
-
 
     # for line in file:
     #
@@ -81,9 +105,9 @@ def filter_file(File_name):
     #
     #     if commentEnd in line:
     #         Comment = False
-        # End of Block comment filter
+    # End of Block comment filter
 
-        # Need to do \n thing here
+    # Need to do \n thing here
 
     # Empty space token filter
     loopCount = 0
@@ -139,10 +163,11 @@ def filter_file(File_name):
     #     loopCount += 1
 
     # print final filtered list
-    for line in lineList:
-        print(line)
+    # for line in lineList:
+    #     print(line)
 
     return lineList
+
 
 # def printToken(fileList, kw, id, op, lit, spS)
 #     for each x in lineList
@@ -162,6 +187,24 @@ def filter_file(File_name):
 #             if lineList[x] == specialSymbols[i]
 #                 print('Next token is:' + specialSymbolsID[i] + 'Next lexeme is ' + lineList[i]
 
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
+def Convert(a):
+    it = iter(a)
+    res_dct = dict(zip(it, it))
+    return res_dct
+
+
+def merge_dictionaries(dict1, dict2):
+    merged_dict = dict1.copy()
+    merged_dict.update(dict2)
+    return merged_dict
 
 
 # group 6 (Allen Smith, Nikita Smith, Josh Poore, Cole Young)
@@ -170,5 +213,48 @@ def filter_file(File_name):
 # TODO: 3. feed in .scl file through argument when calling "main" (ex: "python scl_scanner.py areacir.py" for scanning areacir.py)
 
 if __name__ == '__main__':
-    Area_cir = filter_file("areacir.scl")
+    ItemList = filter_file("areacir.scl")
 
+    finalTokenList = []
+    megaDict = {}
+
+    for Items in ItemList:
+        for TokenItem in Items:
+            # create token object
+            # print to console
+            # save to json
+            if TokenItem in tokenList["keywords"]:
+                newToken = Token('keywords', tokenList["keywords"][TokenItem], TokenItem)
+
+            if TokenItem in tokenList["identifiers"]:
+                newToken = Token('identifiers', tokenList["identifiers"][TokenItem], TokenItem)
+
+            if TokenItem in tokenList["operators"]:
+                newToken = Token('operators', tokenList["operators"][TokenItem], TokenItem)
+
+            if TokenItem in tokenList["specialSymbols"]:
+                newToken = Token('specialSymbols', tokenList["specialSymbols"][TokenItem], TokenItem)
+
+            if TokenItem[0] == '"' and TokenItem[len(TokenItem) - 1] == '"':
+                newToken = Token('literals', 600, TokenItem)
+
+            if isfloat(TokenItem):
+                newToken = Token('literals', 600, TokenItem)
+
+            finalTokenList.append(newToken)
+            print("New Token created: ", newToken.getData())
+
+        newToken = Token('EndOfStatement', 1000, 'EOS')
+        finalTokenList.append(newToken)
+        print("New Token created: ", newToken.getData())
+
+    jsonFile = open("OutputTokens.json", "w")
+
+    for Token in finalTokenList:
+        tokenData = Token.getData()
+        lst = ['Type', tokenData[0], 'id', tokenData[1], 'value', tokenData[2]]
+        newList = Convert(lst)
+        megaDict = megaDict | newList
+
+    json_object = json.dumps(megaDict, indent=4)
+    jsonFile.write(json_object)
