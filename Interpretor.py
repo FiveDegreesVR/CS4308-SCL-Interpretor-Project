564cc8d81c0be89d9cb2053e1bbcb4d31948299c
Joshua Poore <joshua.poore.work@gmail.com>
2023-04-23 04:38:41 -0400

    Update Interpretor.py

--- Interpretor.py
+++ Interpretor.py
@@ -12,28 +12,38 @@ def rightmost(TreeNode root):
         return root.data
 
 def rightfull(TreeNode root, String statement):
-     if(root.right.data is not str()):
-             if(root.right.data is "PI"):
+    if(root.right.data is not str()):
+            if(root.right.data is "PI"):
                   statement = statement + "math.pi"
-             else:
+            else:
                 statement = statement + root.right.data
-             rightfull(root.right, statement)
-        return statement
+            rightfull(root.right, statement)
+    return statement
              
-
+# Loop_bit is to make sure you remember the indent for the loop until endloop statement appears
 def convert(TreeNode root):
     Converted = []
+    Loop_bit = 0
     if(root.data is not int):
-         if(root.data is "define"):
-              if(root.right.data  is "N"):
-                   Converted.append("N = 100")
-              else:
-                   Converted.append(root.right.data + "= 0")
-            elif(root.data is "input"):
-              Converted.append(rightmost(root) + "= int(input("+ root.right.data+"))")
-            elif(root.data is "set"):
-              Converted.append(rightfull(root,str()))
-            elif(root.data is "symbol"):
-              Converted.append("import math")
-            elif(root.data is "display"):
-              
+        if(root.data is "define"):
+            if(root.right.data  is "N"):
+                Converted.append("N = 100")
+            elif(root.right.data is "varr"):
+                 Converted.append("varr = [0] * N")
+            else:
+                Converted.append(root.right.data + "= 0")
+        elif(root.data is "input"):
+            if(rightmost(root) is "num"):
+                Converted.append(rightmost(root) + "= int(input("+ root.right.data+"))")
+            else:
+                Converted.append(rightmost(root) + "= float(input("+ root.right.data+"))")
+        elif(root.data is "set"):
+            Converted.append(rightfull(root,str()))
+        elif(root.data is "symbol"):
+            Converted.append("import math")
+        elif(root.data is "display"):
+            Converted.append("print("+root.right.data+","+rightmost(root)+")")
+        elif(root.data is "for"):
+            Converted.append("for j in range(num):")
+            Loop_bit = 1
+
